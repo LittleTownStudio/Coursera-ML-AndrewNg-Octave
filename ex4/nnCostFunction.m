2188d78baa9280bb7c9f4f0db693fbef1ee64808
yuandecheng <LittleTownStudio@163.com>
2021-07-09 22:39:29 +0800

    delete old code

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -61,46 +61,6 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
-
-##h = eye(num_labels);
-##y = h(y, :);
-##
-##a1 = [ones(m, 1) X];
-##
-##z2 = a1*Theta1';
-##
-##a2 = sigmoid(z2);
-##
-##a2 = [ones(m, 1) a2];
-##
-##z3 = a2*Theta2';
-##
-##a3 = sigmoid(z3);
-##
-##J = -1/m*sum(sum(y.*log(a3) + (1 - y).*log(1 - a3))) + lambda/(2*m)*(sum(sum(Theta1(:, 2:end).^2)) + sum(sum(Theta2(:, 2:end).^2)));
-##
-## %-------------------------------------------------------------
-##
-##d_3 = (a3 - y);
-##
-##d_2 = d_3*Theta2;
-##d_2 = d_2(:, 2:end);
-##d_2 = d_2.*sigmoidGradient(z2);
-##
-##delta_2 = zeros(size(Theta2));
-##delta_2 = delta_2 + d_3'*a2;
-##
-##Theta2_grad = ((1/m)*delta_2) + ((lambda/m)*Theta2);
-##
-##
-##delta_1 = zeros(size(Theta1));
-##delta_1 = delta_1 + d_2'*a1;
-##
-##Theta1_grad = ((1/m)*delta_1) + ((lambda/m)*Theta1);
-##
-##Theta1_grad(:,1) = Theta1_grad(:,1) - ((lambda/m)*(Theta1(:,1)));
-##Theta2_grad(:,1) = Theta2_grad(:,1) - ((lambda/m)*(Theta2(:,1)));
-
 % =========================================================================
 Y = [];
 E = eye(num_labels);
@@ -120,7 +80,7 @@ temp2 = [zeros(size(Theta2, 1), 1) Theta2(:, 2:end)];
 temp1 = sum(temp1 .^2);
 temp2 = sum(temp2 .^2);
 
-cost = Y .* log(a3) + (1 -Y) .* log(1- a3);
+cost = Y .* log(a3) + (1 - Y) .* log(1 - a3);
 J = -1 / m * sum(cost(:)) + lambda/(2*m) * (sum(temp1(:)) + sum(temp2(:)));
 
 
@@ -150,17 +110,13 @@ for t = 1:m
     delta_1 = delta_1 + err_2 * a_1';
 end
 
-
 Theta1_temp = [zeros(size(Theta1, 1), 1)  Theta1(:, 2:end)];
 Theta2_temp = [zeros(size(Theta2, 1), 1)  Theta2(:, 2:end)];
 
 Theta1_grad = 1/m * delta_1 + lambda/m *Theta1_temp;
 Theta2_grad = 1/m * delta_2 + lambda/m *Theta2_temp;
 
-
-
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
 
 end
